101fd8467d48f6c7cc9a15fc6bdf29ee80884244
mehdi eloualy <mehdieloualy@gmail.com>
2022-11-23 23:09:48 +0100

    Fix deployement issues

--- server/database/index.ts
+++
@@ -1,7 +0,0 @@
-// import mysql from "mysql2";
-
-// export default  function connectDatabase(){
-//   const connection = mysql.createConnection(process.env.DATABASE_URL as string);
-//   console.log("Connected to PlanetScale!");
-//   connection.end();
-// };

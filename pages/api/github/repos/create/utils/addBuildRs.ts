38303da5d5603b16214a13793a08822adadda8b2
mehdi eloualy <mehdieloualy@gmail.com>
2022-12-03 19:34:41 +0100

    Add src-tauri/src/main.rs file

--- pages/api/github/repos/create/utils/addBuildRs.ts
+++ pages/api/github/repos/create/utils/addBuildRs.ts
@@ -11,5 +11,26 @@ const addBuildRs = async (octokit, owner: string, repo: string) => {
     message: "Add build.rs",
     content: Buffer.from(buildRs).toString("base64"),
   });
+  await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+    owner: owner,
+    repo: repo,
+    path: "src-tauri/src/main.rs",
+    message: "Add main.rs",
+    content: Buffer.from(`
+    #![cfg_attr(
+        all(not(debug_assertions), target_os = "windows"),
+        windows_subsystem = "windows"
+    )]
+    
+    
+    fn main() {
+        tauri::Builder::default()
+            .run(tauri::generate_context!())
+            .expect("error while running tauri application");
+    }
+    
+    `).toString("base64"),
+    });
+
 };
 export default addBuildRs;

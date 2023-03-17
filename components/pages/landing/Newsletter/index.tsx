d22a9db5b6a65a1d90094827a2bc1fb0fdd03171
Mehdi ELOUALY <92476321+meel-hd@users.noreply.github.com>
2023-03-17 15:30:28 +0100

    Add newsletter ui

---
+++ components/pages/landing/Newsletter/index.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
+import { useState } from "react";
+import { ChevronRight, News } from "tabler-icons-react";
+
+function Newsletter() {
+  const [email, setEmail] = useState("");
+  const alertN = () => {
+    if (email.length > 2) {
+      showNotification({
+        title: "Thank you for subscribing!",
+        message: "We will send you an email when we launch.",
+        color: "violet",
+        icon: <News />,
+      });
+    }
+  };
+  return (
+    <div className="h-40 px-14 flex flex-col sm:flex-row justify-evenly sm:justify-between items-center">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          alertN();
+        }}
+        className="flex gap-3 items-center justify-center"
+      >
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="h-12 w-60 pl-2 rounded-full text-sm bg-white/10"
+          placeholder="Email address"
+        />
+        <Button
+          type="submit"
+          className="w-14 h-14 bg-gradient-to-r  from-indigo-500 to-violet-400 hover:shadow-0xl"
+        >
+          <ChevronRight />
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default Newsletter;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,16 @@
-import { Button, useMantineColorScheme } from "@mantine/core";
-import AlphaRelease from "components/pages/landing/AlphaRelease";
+import { useMantineColorScheme } from "@mantine/core";
 import AlphaReleasePricing from "components/pages/landing/AlphaReleasePricing";
-import Compatibility from "components/pages/landing/Compatibility";
+import SupportedPlatforms from "components/pages/landing/Compatibility/SupportedPlatforms";
 import Features from "components/pages/landing/Features";
 import Footer from "components/pages/landing/Footer";
 import JoinInvitation from "components/pages/landing/Invitation";
-import type { NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import Header from "../components/layout/head";
-import HeroSection from "../components/pages/landing/Hero";
-import Walkthrought from "../components/pages/landing/Walkthrough";
-import { Image } from "@mantine/core";
-import { useEffect } from "react";
-import { useColorScheme } from "@mantine/hooks";
-import SupportedPlatforms from "components/pages/landing/Compatibility/SupportedPlatforms";
 import NewReleases from "components/pages/landing/NewReleases";
 import TheBest from "components/pages/landing/TheBest";
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useEffect } from "react";
+import Header from "../components/layout/head";
+import HeroSection from "../components/pages/landing/Hero";
 
 export const MainColor = "#1C1C28";
 

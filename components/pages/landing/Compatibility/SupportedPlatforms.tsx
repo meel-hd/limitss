5872ba2c7d161f06f993e6aeaa69b9e3b9a67fd0
Mehdi ELOUALY <92476321+meel-hd@users.noreply.github.com>
2023-03-28 12:22:32 +0000

    Fix macos icon image

--- components/pages/landing/Compatibility/SupportedPlatforms.tsx
+++ components/pages/landing/Compatibility/SupportedPlatforms.tsx
@@ -3,15 +3,32 @@ import { MainColor } from "pages";
 
 function SupportedPlatforms() {
   return (
-    <div style={{
+    <div
+      style={{
         //linear gradient
         backgroundImage: `linear-gradient(${MainColor}10,${MainColor}40, ${MainColor} , ${MainColor}40, ${MainColor}10)`,
-        
-    }} className=" h-40 -mt-12 z-50 relative flex justify-center items-centers overflow-hidden">
+      }}
+      className=" h-40 -mt-12 z-50 relative flex justify-center items-centers overflow-hidden"
+    >
       <div className="w-full flex justify-center gap-5 items-center pl-4">
-        <Image width={150} src="/images/Hero/OS/windows.png" alt="Windows" className="select-none pointer-events-none"/>
-        <Image width={150} src="/images/Hero/OS/macOs.png" alt="Mac" className="select-none pointer-events-none"/>
-        <Image width={150} src="/images/Hero/OS/linux.png" alt="Linux" className="select-none pointer-events-none"/>
+        <Image
+          width={150}
+          src="/images/Hero/OS/windows.png"
+          alt="Windows"
+          className="select-none pointer-events-none"
+        />
+        <Image
+          width={150}
+          src="/images/Hero/OS/macOS.png"
+          alt="Mac"
+          className="select-none pointer-events-none"
+        />
+        <Image
+          width={150}
+          src="/images/Hero/OS/linux.png"
+          alt="Linux"
+          className="select-none pointer-events-none"
+        />
       </div>
     </div>
   );
